a83234956dcfa69a408835657b671052a373c6bd
alecxvs <alec.xvs@gmail.com>
2016-11-17 15:11:34 -0600

    Better formatting for guarded modules

--- libraries/conf_helpers.rb
+++ libraries/conf_helpers.rb
@@ -1,58 +1,58 @@
 module CernerSplunk
-  unless defined?(ConfHelpers)
-    # Helper methods for reading and evaluating Splunk config
-    module ConfHelpers
-      def self.evaluate_config(current_config, desired_config)
-        return desired_config.call(current_config) if desired_config.is_a? Proc
-        desired_config.map do |section, props|
-          props.is_a?(Proc) ? props.call(section, current_config[section] || {}) : [section, props]
-        end.to_h
-      end
+  return if defined?(ConfHelpers)
 
-      def self.stringify_config(config)
-        config.map do |section, props|
-          [section.to_s, props.map { |k, v| [k.to_s, v.to_s] }.to_h]
-        end.to_h
-      end
+  # Helper methods for reading and evaluating Splunk config
+  module ConfHelpers
+    def self.evaluate_config(current_config, desired_config)
+      return desired_config.call(current_config) if desired_config.is_a? Proc
+      desired_config.map do |section, props|
+        props.is_a?(Proc) ? props.call(section, current_config[section] || {}) : [section, props]
+      end.to_h
+    end
 
-      def self.read_config(conf_path)
-        conf_file = Pathname.new(conf_path)
-        return {} unless conf_file.exist?
+    def self.stringify_config(config)
+      config.map do |section, props|
+        [section.to_s, props.map { |k, v| [k.to_s, v.to_s] }.to_h]
+      end.to_h
+    end
 
-        parse_config(conf_file)
-      end
+    def self.read_config(conf_path)
+      conf_file = Pathname.new(conf_path)
+      return {} unless conf_file.exist?
 
-      def self.parse_config(conf_body)
-        current_stanza = 'default'
-        current_config = {}
+      parse_config(conf_file)
+    end
 
-        conf_body.each_line do |ln|
-          case ln
-          when /^\s*\[([^#]+)\]\s*/
-            current_stanza = Regexp.last_match[1]
-          when /^\s*([^#]+?)\s*=\s*([^#]+?)\s*(?:#.*)?$/
-            (current_config[current_stanza] ||= {})[Regexp.last_match[1]] = Regexp.last_match[2]
-          end
+    def self.parse_config(conf_body)
+      current_stanza = 'default'
+      current_config = {}
+
+      conf_body.each_line do |ln|
+        case ln
+        when /^\s*\[([^#]+)\]\s*/
+          current_stanza = Regexp.last_match[1]
+        when /^\s*([^#]+?)\s*=\s*([^#]+?)\s*(?:#.*)?$/
+          (current_config[current_stanza] ||= {})[Regexp.last_match[1]] = Regexp.last_match[2]
         end
-
-        current_config
       end
 
-      def self.merge_config(current_config, desired_config)
-        (current_config.keys + desired_config.keys).uniq.each do |section|
-          (current_config[section] ||= {}).merge! desired_config[section] if desired_config[section]
-        end
+      current_config
+    end
 
-        stream = StringIO.new
-        stream.puts '# Warning: This file is managed by Chef!'
-        stream.puts '# Comments will not be preserved and configuration may be overwritten.'
-        current_config.each do |section, props|
-          stream.puts ''
-          stream.puts "[#{section}]"
-          props.each { |key, value| stream.puts "#{key} = #{value}" }
-        end
-        stream.string
+    def self.merge_config(current_config, desired_config)
+      (current_config.keys + desired_config.keys).uniq.each do |section|
+        (current_config[section] ||= {}).merge! desired_config[section] if desired_config[section]
       end
+
+      stream = StringIO.new
+      stream.puts '# Warning: This file is managed by Chef!'
+      stream.puts '# Comments will not be preserved and configuration may be overwritten.'
+      current_config.each do |section, props|
+        stream.puts ''
+        stream.puts "[#{section}]"
+        props.each { |key, value| stream.puts "#{key} = #{value}" }
+      end
+      stream.string
     end
   end
 end
--- libraries/path_helpers.rb
+++ libraries/path_helpers.rb
@@ -1,48 +1,48 @@
 module CernerSplunk
-  unless defined? PathHelpers
-    # Helper methods for Splunk-related file and URL paths
-    module PathHelpers
-      # Provides constant defaults for Splunk's various default install directories based on platform and package.
-      # Returned hash is nested by package and os. For example, `default_install_dirs[:splunk][:linux]` returns
-      # the default directory for Splunk on Linux.
-      #
-      # @return [Hash] an index of default install directories for Splunk and the Universal Forwarder
-      def self.default_install_dirs
-        {
-          splunk: {
-            linux: '/opt/splunk',
-            windows: 'c:\Program Files\Splunk'
-          },
-          universal_forwarder: {
-            linux: '/opt/splunkforwarder',
-            windows: 'c:\Program Files\SplunkUniversalForwarder'
-          }
+  return if defined? PathHelpers
+  
+  # Helper methods for Splunk-related file and URL paths
+  module PathHelpers
+    # Provides constant defaults for Splunk's various default install directories based on platform and package.
+    # Returned hash is nested by package and os. For example, `default_install_dirs[:splunk][:linux]` returns
+    # the default directory for Splunk on Linux.
+    #
+    # @return [Hash] an index of default install directories for Splunk and the Universal Forwarder
+    def self.default_install_dirs
+      {
+        splunk: {
+          linux: '/opt/splunk',
+          windows: 'c:\Program Files\Splunk'
+        },
+        universal_forwarder: {
+          linux: '/opt/splunkforwarder',
+          windows: 'c:\Program Files\SplunkUniversalForwarder'
         }
-      end
+      }
+    end
 
-      # Utility method for splitting the filename from a URL.
-      #
-      # @param url [String]
-      # @return [String] the filename at the end of the URL
-      def self.filename_from_url(url)
-        Pathname.new(URI.parse(url).path).basename.to_s
-      end
+    # Utility method for splitting the filename from a URL.
+    #
+    # @param url [String]
+    # @return [String] the filename at the end of the URL
+    def self.filename_from_url(url)
+      Pathname.new(URI.parse(url).path).basename.to_s
+    end
 
-      # Append the relative path of Splunk's version file to the home directory path
-      #
-      # @param splunk_home [String] the absolute path to Splunk's home directory
-      # @return [Pathname] the absolute path to Splunk's version file
-      def self.version_pathname(splunk_home)
-        Pathname.new(splunk_home).join('etc/splunk.version')
-      end
+    # Append the relative path of Splunk's version file to the home directory path
+    #
+    # @param splunk_home [String] the absolute path to Splunk's home directory
+    # @return [Pathname] the absolute path to Splunk's version file
+    def self.version_pathname(splunk_home)
+      Pathname.new(splunk_home).join('etc/splunk.version')
+    end
 
-      # Append the relative path of Splunk's first-time-run marker to the home directory path
-      #
-      # @param splunk_home [String] the absolute path to Splunk's home directory
-      # @return [Pathname] the absolute path to Splunk's first-time-run marker file
-      def self.ftr_pathname(splunk_home)
-        Pathname.new(splunk_home).join('ftr')
-      end
+    # Append the relative path of Splunk's first-time-run marker to the home directory path
+    #
+    # @param splunk_home [String] the absolute path to Splunk's home directory
+    # @return [Pathname] the absolute path to Splunk's first-time-run marker file
+    def self.ftr_pathname(splunk_home)
+      Pathname.new(splunk_home).join('ftr')
     end
   end
 end
