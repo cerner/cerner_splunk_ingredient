9a0f4d56dc210c1932dd2ce16e167336842d6ead
alecxvs <alec.xvs@gmail.com>
2017-04-18 13:59:49 -0500

    1012

--- libraries/splunk_version.rb
+++ libraries/splunk_version.rb
@@ -29,6 +29,9 @@ module CernerSplunk
     end
 
     def prerelease?
+      Chef::Log.warn @meta
+      Chef::Log.warn @meta.empty?
+      Chef::Log.warn !@meta.empty?
       !@meta.empty?
     end
 
