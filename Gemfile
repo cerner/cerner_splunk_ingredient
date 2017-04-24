37b7de5ec146ea63c205a36f0422330812db3f85
alecxvs <alec.xvs@gmail.com>
2017-04-24 14:32:52 -0500

    Update kitchen.yml, Gemfile, and metadata.rb

--- Gemfile
+++ Gemfile
@@ -2,20 +2,18 @@
 
 source 'https://rubygems.org'
 
-gem 'berkshelf', '~> 5.3'
-gem 'chef', '~> 12.13', ENV['CHEF_VERSION'] || '<= 12.13.37' # Allow testing both versions of Chef
-gem 'chefspec', '~> 5.3'
-gem 'fauxhai', '~> 3.10'
-gem 'foodcritic', '~> 8.0'
-gem 'hashie', '~> 3.4.6'
+gem 'berkshelf', '~> 5.6'
+gem 'chef', '~> 12.14'
+gem 'chefspec', '~> 6.0'
+gem 'fauxhai', '~> 4.1'
+gem 'foodcritic', '~> 10.4'
 gem 'parallel_tests'
-gem 'rubocop', '~> 0.47'
+gem 'rubocop', '~> 0.48'
 
 group :local do
-  gem 'guard', '~> 2.14'
   gem 'kitchen-inspec'
   gem 'kitchen-vagrant'
-  gem 'test-kitchen', '~> 1.14'
-  gem 'winrm', '~> 2.1'
+  gem 'test-kitchen', '~> 1.16'
+  gem 'winrm', '~> 2.2'
   gem 'winrm-fs', '~> 1.0'
 end
--- metadata.rb
+++ metadata.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
-name             'cerner_splunk_ingredient'
-maintainer       'Cerner Innovation, Inc.'
-maintainer_email 'splunk@cerner.com'
-license          'Apache 2.0'
-description      'Installs and configures Splunk'
+name              'cerner_splunk_ingredient'
+maintainer        'Cerner Innovation, Inc.'
+maintainer_email  'splunk@cerner.com'
+license           'Apache-2.0'
+description       'Installs and configures Splunk'
 long_description((Pathname.new(__FILE__).dirname + 'README.md').read)
-version          '0.1.0'
+version           '0.1.0'
 
-source_url       'https://github.com/cerner/cerner_splunk_ingredient'
-issues_url       'https://github.com/cerner/cerner_splunk_ingredient/issues'
+source_url        'https://github.com/cerner/cerner_splunk_ingredient'
+issues_url        'https://github.com/cerner/cerner_splunk_ingredient/issues'
 
-supports         'redhat', '>= 5.5'
-supports         'ubuntu', '>= 12.04'
-supports         'windows', '>= 6.1'
+chef_version      '~> 12.14'
 
-depends          'poise-archive', '~> 1.3.0'
+supports          'redhat', '>= 6.8'
+supports          'ubuntu', '>= 12.04'
+supports          'windows', '>= 6.1'
+
+depends           'poise-archive', '~> 1.3.0'
