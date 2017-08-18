d796a9024a997db7d3199601c20328fa1a0b28e3
alecxvs <alec.xvs@gmail.com>
2017-08-18 13:55:07 -0500

    temp_path??

--- resources/splunk_app.rb
+++ resources/splunk_app.rb
@@ -180,10 +180,8 @@ class PackagedApp < SplunkApp
 
     backup_app if changed? :version
 
-    temp_path = (app_cache_path + 'new').to_s
-
     extraction_resource = poise_archive package_path.to_s do
-      destination temp_path
+      destination((app_cache_path + 'new').to_s)
       user current_owner
       group current_owner
       strip_components 0
